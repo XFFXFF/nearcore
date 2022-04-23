f7b8698db4cccb3ae81bdbff0d00b959e1aa0341
JC Near <jc_near@example.com>
2022-04-22 19:43:44 -0700

    add chunk info debug page

--- chain/client/src/client_actor.rs
+++ chain/client/src/client_actor.rs
@@ -826,6 +826,7 @@ impl Handler<Status> for ClientActor {
                         .to_rfc3339(),
                     validators: validators.to_vec(),
                 },
+                chunk_info: self.client.detailed_upcoming_blocks_info_as_web(),
             })
         } else {
             None
@@ -1787,7 +1788,7 @@ impl ClientActor {
                 .unwrap_or(0),
             self.client.chain.store().get_store_statistics(),
         );
-        debug!(target: "stats", "{}", self.client.detailed_upcoming_blocks_info().unwrap_or(String::from("Upcoming block info failed.")));
+        debug!(target: "stats", "{}", self.client.detailed_upcoming_blocks_info_as_printable().unwrap_or(String::from("Upcoming block info failed.")));
     }
 }
 
